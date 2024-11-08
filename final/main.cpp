29db8b3ca534d4cbfad606904416f17ae335495e
cocoatehcat <147646449+cocoatehcat@users.noreply.github.com>
2024-11-08 11:09:57 -0500

    Word done during class, starting to reconstruct main

---
+++ final/main.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+#include <fstream>
+
+#include <external/glad.h>
+#include <external/stb_image.h>
+#include <external/ewMath.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// IMGUI
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+const int SCREEN_WIDTH = 1080;
+const int SCREEN_HEIGHT = 720;
+
+int main() {
+	printf("Initializing...");
+	if (!glfwInit()) {
+		printf("GLFW failed to init!");
+		return 1;
+	}
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Lighting", NULL, NULL);
+	if (window == NULL) {
+		printf("GLFW failed to create window");
+		return 1;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGL(glfwGetProcAddress)) {
+		printf("GLAD Failed to load GL headers");
+		return 1;
+	}
+
+
+	//Render loop
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+
+	}
+	printf("Shutting down!. . .");
+}
