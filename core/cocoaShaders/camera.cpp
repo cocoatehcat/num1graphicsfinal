f3777774c1b25bd0028f5e7f787238bf71cfc27e
cocoatehcat <147646449+cocoatehcat@users.noreply.github.com>
2024-12-10 19:33:29 -0500

    Uploaded the camera class that I made from my previous project. Going to add in my shaders then review for what this project needs and edit.

---
+++ core/cocoaShaders/camera.cpp
@@ -0,0 +1,93 @@
+#include "camera.h"
+
+namespace cam {
+	void Camera::processInput(GLFWwindow* window, float dt) {
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+			glfwSetWindowShouldClose(window, true);
+		}
+
+		float cameraSpeed = 10.0f * dt; // can be adjusted
+		if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+			cameraSpeed *= 2;
+		}
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+			cameraPos += cameraSpeed * cameraFront;
+		}
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+			cameraPos -= cameraSpeed * cameraFront;
+		}
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+			cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+		}
+		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+			cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+		}
+		if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+			cameraPos += glm::normalize(glm::cross(cameraFront, cameraRight)) * cameraSpeed;
+		}
+		if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
+			cameraPos -= glm::normalize(glm::cross(cameraFront, cameraRight)) * cameraSpeed;
+		}
+
+	}
+
+	// Function to create a random vec3
+	glm::vec3 Camera::createPos() {
+		float x, y, z;
+		x = ew::RandomRange(-10.0f, 10.0f);
+		y = ew::RandomRange(-10.0f, 10.0f);
+		z = ew::RandomRange(-10.0f, 10.0f);
+		return glm::vec3(x, y, z);
+	}
+
+	// Function to show mouse positioning
+	void Camera::mouse_callback(GLFWwindow* window, double xposIn, double yposIn) {
+		float xpos = static_cast<float>(xposIn);
+		float ypos = static_cast<float>(yposIn);
+
+		if (firstMouse) {
+			lastX = xpos;
+			lastY = ypos;
+			firstMouse = false;
+		}
+
+		float xoffset = xpos - lastX;
+		float yoffset = lastY - ypos; // y coords go bottom to top
+		lastX = xpos;
+		lastY = ypos;
+
+		float sensitivity = 0.1f;
+		xoffset *= sensitivity;
+		yoffset *= sensitivity;
+
+		yaw += xoffset;
+		pitch += yoffset;
+
+		// Clamping
+		if (pitch > 89.0f) {
+			pitch = 89.0f;
+		}
+		if (pitch < -89.0f) {
+			pitch = -89.0f;
+		}
+
+		glm::vec3 direction;
+		direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+		direction.y = sin(glm::radians(pitch));
+		direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+		cameraFront = glm::normalize(direction);
+		cameraRight = glm::normalize(glm::cross(cameraUp, cameraFront)); // look into more
+	}
+
+	// Making it zoom
+	void Camera::scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+		fov -= (float)yoffset;
+		// Clamping
+		if (fov < 1.0f) {
+			fov = 1.0f;
+		}
+		if (fov > 120.0f) {
+			fov = 120.0f;
+		}
+	}
+}
---
+++ core/cocoaShaders/camera.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "../external/glad.h"
+#include "../external/ewMath.h"
+#include <math.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace cam {
+	class Camera {
+	public:
+		void processInput(GLFWwindow* window, float dt);
+		glm::vec3 createPos();
+		void mouse_callback(GLFWwindow* window, double xposIn, double yposIn);
+		void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+		glm::vec3 getPos() { return cameraPos; }
+		glm::vec3 getFront() { return cameraFront; }
+		glm::vec3 getUp() { return cameraUp; }
+		float getFOV() { return fov; }
+		glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+
+	private:
+
+		glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+		glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+		glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+		glm::vec3 cameraRight = glm::normalize(glm::cross(cameraUp, cameraDirection));
+
+		bool firstMouse = true;
+		float yaw = -90.0f;
+		float pitch = 0.0f;
+		float lastX = 1080.0f / 2.0;
+		float lastY = 720.0f / 2.0;
+		float fov = 60.0f;
+	};
+}
